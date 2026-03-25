15cb270497cc5e447c8788613b215657dd89df2b
MR ROHIT <mrrohitt22@gmail.com>
2026-03-26 00:59:58 +0530

    fix: revert authDomain to default firebase domain to fix 404 login error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-3260479641-beea3",
   "appId": "1:37774020923:web:7867444b01b67ed285e71e",
   "apiKey": "AIzaSyALf06iClxbQQndo172LuvDiEou5fpWFmk",
-  "authDomain": "ripler-ai.vercel.app",
+  "authDomain": "studio-3260479641-beea3.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "37774020923"
 };
